5abf56dd44acea1f525325ea624d9fbf7d0163f0
Jiawei Xiao <jiawxiao@gmail.com>
2023-04-12 13:22:16 +0800

    refactor: main options

--- src/main.cpp
+++ src/main.cpp
@@ -21,26 +21,24 @@ int main(int argc, char *argv[])
 {
   try {
     po::options_description desc{ "Options" };
-    desc.add_options()("help,h", "Help screen")("verbose,v", "verbose")(
-      "mode,m", po::value<std::string>(), "client or server mode")("server-addr,a",
-      po::value<std::string>()->default_value("192.168.12.2"),
-      "Server's IPoIB addr")("port,p",
-      po::value<std::string>()->default_value("12345"),
-      "server port")("indexfile,i", po::value<std::string>(), "path to .idx file")(
-      "edgefile,e", po::value<std::string>(), "path to .adj file")("kernel,k",
-      po::value<std::string>()->default_value("print_graph"),
-      "kernel to run")("ofile,o",
-      po::value<std::string>()->default_value("/home/username/graphs/kernel-out.txt"),
-      "path to output file")("threads,t",
-      po::value<unsigned long>()->default_value(0),
-      "Number of Worker threads. 0 for all available cores")(
+    desc.add_options()(
+      "help,h", "Help screen")(
+      "verbose,v", "verbose")(
+      "mode,m", po::value<std::string>(), "client or server mode")(
+      "server-addr,a", po::value<std::string>()->default_value("192.168.12.2"), "Server's IPoIB addr")(
+      "port,p", po::value<std::string>()->default_value("12345"), "server port")(
+      "indexfile,i", po::value<std::string>(), "path to .idx file")(
+      "edgefile,e", po::value<std::string>(), "path to .adj file")(
+      "kernel,k", po::value<std::string>()->default_value("print_graph"), "kernel to run")(
+      "ofile,o", po::value<std::string>()->default_value("/home/username/graphs/kernel-out.txt"), "path to output file")(
+      "threads,t", po::value<unsigned long>()->default_value(0), "Number of Worker threads. 0 for all available cores")(
       "print-table", "Print full vertex table after kernel")(
       "kcore-k", po::value<uint32_t>()->default_value(100), "The k in k-core")(
       "delta", po::value<uint32_t>()->default_value(25), "delta step divisor for MIS")(
       "start-vertex", po::value<uint32_t>()->default_value(1), "start vertex for bfs")(
-      "hp", "use huge pages")("double-buffer", "use double buffering")("edgewindow",
-      po::value<uint32_t>()->default_value(1),
-      "how many times larger than max outdegree the edgewindow should be")(
+      "hp", "use huge pages")(
+      "double-buffer", "use double buffering")(
+      "edgewindow", po::value<uint32_t>()->default_value(1), "how many times larger than max outdegree the edgewindow should be")(
       "no-numa-bind", "don't do numa bind");
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
